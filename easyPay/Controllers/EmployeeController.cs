64f0bcb24bbf843d3d2ab55bc10748d672185545
Siyad Adan <siyad.adan94@gmail.com>
2020-07-22 23:34:19 -0400

    Index - EmployeeController

---
+++ easyPay/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using easyPay.Models;
+using easyPay.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace easyPay.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DateJoined
+            }).ToList();
+
+            return View(employees);
+        }
+    }
+}
